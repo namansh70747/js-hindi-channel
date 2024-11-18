2748daa871d6382dc9645535e2ff16453fb1a4e6
Naman Sharma <namsh70747@gmail.com>
2024-11-18 12:47:19 +0530

    variable,constant,let

---
+++ 01_Basics/01.variables.js
@@ -0,0 +1,16 @@
+const account_Id = 144553
+let account_Email = "namsh70747@gmail.com"
+var account_Password = "12345"
+account_City = "Jaipur" //Don't Use This
+let accountState
+/*
+Prefer not use var Because of issue in block scopeand functional scope
+*/
+// account_Id = 2 // not allowed
+account_Email = "namansh70747@gmail.com"
+account_Password = "212121"
+account_City = "Delhi"
+console.log(account_Id);
+console.log(account_Email);
+console.table([account_Id,account_Password,account_City, account_Email,accountState])
+
