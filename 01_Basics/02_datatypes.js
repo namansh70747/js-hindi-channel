b5cb13942891d19c47b6e34b50b269c407588d61
Naman Sharma <namsh70747@gmail.com>
2024-11-18 15:02:47 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3) // we are1454546i8 using node js not browser
+console.log(3+3) 
+console.log("Hitesh")
+let name = "Naman Sharma" // 2 to power 53
+let age = 18 // int
+//BigInt
+let isloggedIn = false //true or false 
+let state;
+let noofPeoples = null //standlone data type 
+// number => 2 to power 53
+//bigint
+//string => true/false 
+//null => standalone data type
+//undefined
+//symbol => unique
+console.table([name , age , isloggedIn , state , noofPeoples])
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof noofPeoples); 
+
+
+
