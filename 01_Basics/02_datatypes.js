59a7882bafd855f667918974828fdaff4d5d5229
Naman Sharma <namsh70747@gmail.com>
2024-11-20 00:42:55 +0530

    Conversion and Operations

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -22,3 +22,5 @@ console.log(typeof noofPeoples);
 
 
 
+
+
---
+++ 01_Basics/03conversionandoperations.js
@@ -0,0 +1,164 @@
+// let score1 = "33bc"
+
+// console.log(typeof score1)
+// console.log(typeof(score1))
+
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1)
+
+// let score2 = null
+
+// console.log(typeof score2)
+// console.log(typeof(score2))
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+
+// let score3 = undefined
+
+// console.log(typeof score3)
+// console.log(typeof(score3))
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
+
+// let score4 = true
+
+// console.log(typeof score4)
+// console.log(typeof(score4))
+
+// let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
+
+// let score5 = false
+
+// console.log(typeof score5)
+// console.log(typeof(score5))
+
+// let valueInNumber5 = Number(score5)
+// console.log(typeof valueInNumber5)
+// console.log(valueInNumber5)
+
+// // "33" => 33
+// // "33" => NaN
+// // true => 1 ; fale => 0
+
+// let isLoggedIn1 = 44
+
+// console.log(typeof isLoggedIn)
+// let coversionsIn = Boolean(isLoggedIn1)
+// console.log(typeof coversionsIn)
+// console.log(coversionsIn)
+
+// //1=>true; false=>false 
+// //""=>false
+// //"hitesh"=>true
+
+// let somenumber = 33
+
+// let stringnumber = String(somenumber)
+// console.log(typeof stringnumber)
+// console.log(stringnumber)
+
+// let number = 66
+// console.log(typeof number);
+// let Instring = String(number);
+// console.log(typeof Instring);
+// let Inbool = Boolean(number)
+// console.log(Inbool)
+// //conclusion 
+// //Is that any number other than 0 have true as a value and one and only 0 have false value please not it 
+
+// let booleanIn = true
+// console.log(typeof booleanIn)
+// let InString2 = String(booleanIn)
+// console.log(typeof InString2)
+// console.log(InString2);
+
+// let strinIn = "Naman Sharma"
+// let StringInt = Number (strinIn)
+// console.log(typeof StringInt);
+// console.log(StringInt);
+// let Inbool2 = Boolean(strinIn)
+// console.log(Inbool2);
+// let InInt = Number (strinIn)
+// console.log(typeof InInt);
+// console.log(InInt);
+// Conclusion is that Naman Sharma in Number is Nan(Nota Number)
+// Conclusion is that In Boolean it is true
+// Boolean in string is Boolean itself
+// Null is Object
+// And Converting it Into integer we get the 0
+// Undefined is Undefined 
+// let EmptyString = ""
+// let intointeger = Number(EmptyString)
+// console.log(intointeger);
+// *********************** Operations************************
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3);
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = "Hitesh"
+
+// let str3 = str1 + " "+str2
+// console.log(str3);
+
+str4 = 2+"1"
+console.log(typeof str4)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log(1+2+"2");
+console.log("1"+(2+2));
+// Conclusion 
+/* What will be First Calculation will be done according to that 
+like when we have string first than 
+ex - 
+"1"+2+2=> 122
+
+like when we have number first then 
+ex -
+2+2+"1"=> 41
+*/
+
+console.log(+true)
+console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1,num2,num3);
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+// Conclusion 
+// Ex ->
+// Preffix and Postffix has a Significant difference which means that the preffix adds first and post fix add after 
+let x = 3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+let b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
