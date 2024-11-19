dc3bf48d9dc76a26e897247a325021af4c9d334d
Naman Sharma <namsh70747@gmail.com>
2024-11-20 01:27:14 +0530

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,36 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log("2" == 2)
+// and == null becomes undefined and when we check the equality we get false as the answer
+// and for other operator we use normal style operations
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// undefined everytime gives NaN that's Why Always it is False 
+// thus Every Above Statement have answer false 
+// in middle example treat undefined as null and give answer according to that and the answer we get in null is 0 and thus here we get false 
+//===
+
+console.log("2" === 2);
+//=== Is used for Strictly Checking a value 
+// Means it checks the datatypes also of the objets
+console.log(undefined == null);
+
+
+
+
+
+
+
+
+
+
+
+
